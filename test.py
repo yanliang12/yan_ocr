9ae9f236053b3c47a42ac168b9bbb72ad193dc1f
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-10-14 11:40:20 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+
+#############test.py##########
 import easyocr
+
+print('chinese example')
 reader = easyocr.Reader(['ch_sim','en']) 
-print(reader.readtext('chinese_ocr.jpeg'))
+o = reader.readtext('chinese_ocr.jpeg')
+for r in o:
+	print(r)
+
+print('arabic example')
+reader = easyocr.Reader(['ar','en']) 
+o = reader.readtext('arabic_road_sign.jpg')
+for r in o:
+	print(r)
+#############test.py##########
