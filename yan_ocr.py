6d13789c8a4665dda50eecb3ef2e56598cb9c707
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-10-15 16:01:31 +0400

    Create yan_ocr.py

---
+++ yan_ocr.py
@@ -0,0 +1,39 @@
+############yan_ocr.py##########
+import numpy
+import easyocr
+from PIL import Image, ImageDraw, ImageFont
+
+fnt = ImageFont.truetype("MuseoSans_500.otf", 40)
+reader_en = easyocr.Reader(['en']) 
+
+def extract_text(
+	image_path,
+	output_image_path):
+	output = []
+	o = reader_en.readtext(image_path)
+	im = Image.open(image_path)
+	draw = ImageDraw.Draw(im)
+	for r in o:
+		text = r[1]
+		score = r[2]
+		post = [tuple(p) for p in r[0]]
+		line1 = post[0]+post[1]
+		line2 = post[1]+post[2]
+		line3 = post[2]+post[3]
+		line4 = post[3]+post[0]
+		draw.line(line1, fill=128, width = 5)
+		draw.line(line2, fill=128, width = 5)
+		draw.line(line3, fill=128, width = 5)
+		draw.line(line4, fill=128, width = 5)
+		#draw.text(post[3], text)
+		try:
+			draw.text(post[3], text, font = fnt) #
+		except:
+			pass 
+		coordinate = [list(p) for p in r[0]]
+		tag = {"text": text, "score": score, 
+		"coordinate": coordinate}
+		output.append(tag)
+	im.save(output_image_path)
+	return output
+############yan_ocr.py##########
